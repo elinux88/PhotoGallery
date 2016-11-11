8157a7bde5800151c92d04b235e7507fb445809a
Eli Cook <elinux88@gmail.com>
2016-11-10 20:26:02 -0700

    Added KeyHolder class without API Key

---
+++ app/src/main/java/site/elioplasma/ecook/photogallery/KeyHolder.java
@@ -0,0 +1,12 @@
+package site.elioplasma.ecook.photogallery;
+
+/**
+ * Created by eli on 4/13/16.
+ * Updated by eli on 11/10/16.
+ */
+public class KeyHolder {
+    private static final String API_KEY = "<PUT_API_KEY_HERE>";
+    public static String getApiKey() {
+        return API_KEY;
+    }
+}
